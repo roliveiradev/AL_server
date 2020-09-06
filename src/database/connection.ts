d546c75474b547da8b8d0ffe13c560838b2e5269
Rodrigo Rodrigues <roliveiradev@gmail.com>
2020-09-06 20:58:10 -0300

    Primeiro commit Aplicacao

---
+++ src/database/connection.ts
@@ -0,0 +1,14 @@
+import knex from 'knex';
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : process.env.PG_HOST,
+    user : process.env.PG_USER,
+    password : process.env.PG_PASSWORD,
+    database : process.env.PG_DATABASE,
+  },
+  useNullAsDefault: true,
+});
+
+export default db;
